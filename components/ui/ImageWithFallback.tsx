e2cea8b7e8a430e2ac24eb659dc7b74769949938
mahdi <falahati.mahdi@yahoo.com>
2025-05-23 19:58:53 +0330

    refactor: support dynamic aspect ratio to control image height

--- components/ui/ImageWithFallback.tsx
+++ components/ui/ImageWithFallback.tsx
@@ -7,22 +7,33 @@ import { shimmer, toBase64 } from '@/utils/image';
 export default function ImageWithFallback({
   src,
   alt,
+  aspectRatio = '2 / 1', // default
+  sizes = '100vw',
+
 }: {
   src: string;
   alt: string;
+  aspectRatio?: string;
+  sizes?: string;
+
 }) {
   const [error, setError] = useState(false);
 
   return (
-    <div style={{ position: 'relative', width: '100%', aspectRatio: '3 / 2' }}>
+    <div style={{
+      position: 'relative',
+      width: '100%',
+      aspectRatio,
+    }}>
       <Image
         src={error ? '/not-found.png' : src}
         alt={alt}
         fill
         placeholder="blur"
+        sizes={sizes}
         blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(500, 300))}`}
         onError={() => setError(true)}
-        style={{ objectFit: 'cover', borderRadius: 8 }}
+        style={{ objectFit: 'cover' }}
       />
     </div>
   );
